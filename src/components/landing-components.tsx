03614deef39d3e43f5e43882efb33639bb9922de
Juan Pablo Gómez <juanpgrangel117@gmail.com>
2025-01-15 20:01:00 -0500

    Para revisar

--- src/components/landing-components.tsx
+++ src/components/landing-components.tsx
@@ -93,12 +93,12 @@ export const Hero: React.FC = () => {
             <div className="grid min-h-screen px-8">
                 <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
                     <ScrollAnimation animation="zoomIn">
-                        <Typography variant="h1" color="white" className="lg:max-w-3xl">
+                        <Typography variant="h1" color="white" className="lg:max-w-3xl" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                             CHIZU
                         </Typography>
                     </ScrollAnimation>
                     <ScrollAnimation animation="fadeIn">
-                        <Typography variant="h3" color="white" className="mb-2">
+                        <Typography variant="h3" color="white" className="mb-2" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                             Level Up Your Vision
                         </Typography>
                     </ScrollAnimation>
@@ -106,8 +106,7 @@ export const Hero: React.FC = () => {
                         <Typography
                             variant="lead"
                             color="white"
-                            className="mt-1 mb-12 w-full md:max-w-full lg:max-w-2xl"
-                        >
+                            className="mt-1 mb-12 w-full md:max-w-full lg:max-w-2xl" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                        >
                             Únete a la plataforma que fortalece tus sueños
                         </Typography>
                     </ScrollAnimation>
@@ -133,15 +132,14 @@ export const AboutEvent: React.FC = () => {
     return (
         <section id="about-us" className="container mx-auto flex flex-col items-center px-4 py-10 scroll-mt-10">
             <ScrollAnimation animation="fadeIn">
-                <Typography variant="h3" className="text-center" color="blue-gray">
+                <Typography variant="h3" className="text-center" color="blue-gray" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                     Sobre Nosotros
                 </Typography>
             </ScrollAnimation>
             <ScrollAnimation animation="fadeIn">
                 <Typography
                     variant="lead"
-                    className="mt-2 lg:max-w-4xl mb-8 w-full text-center font-normal !text-black-500"
-                >
+                    className="mt-2 lg:max-w-4xl mb-8 w-full text-center font-normal !text-black-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                >
                     Somos una plataforma que impulsa la creatividad, permite la financiación
                     colaborativa y ofrece un espacio donde los sueños de los creadores se convierten en
                     experiencias únicas para los jugadores. Aquí apoyas, descubres y juegas como nunca antes 🎮✨
@@ -183,10 +181,10 @@ export const OurStats: React.FC = () => {
         <section id="advantages" className="container mx-auto px-8 py-20 grid gap-8">
             <ScrollAnimation animation="fadeIn">
                 <div className="bg-gray-100 rounded-xl shadow-md p-8">
-                    <Typography className="text-5xl font-bold leading-tight" color="blue-gray">
+                    <Typography className="text-5xl font-bold leading-tight" color="blue-gray"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                         Ventajas
                     </Typography>
-                    <Typography variant="lead" className="mt-3 text-gray-500 text-justify text-xl">
+                    <Typography variant="lead" className="mt-3 text-gray-500 text-justify text-xl"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                         En Chizu, buscamos revolucionar la forma en que los videojuegos son
                         creados. Nuestra plataforma ofrece beneficios únicos tanto a
                         desarrolladores independientes como a jugadores, creando un
@@ -201,10 +199,10 @@ export const OurStats: React.FC = () => {
                 {STATS.map((props, key) => (
                     <ScrollAnimation key={key} animation="zoomIn">
                         <div className="bg-gray-100 rounded-xl shadow-md p-6 flex flex-col gap-2">
-                            <Typography className="text-2xl font-bold" color="blue-gray">
+                            <Typography className="text-2xl font-bold" color="blue-gray" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                                 {props.count}
                             </Typography>
-                            <Typography variant="paragraph" className="text-gray-500 text-xl">
+                            <Typography variant="paragraph" className="text-gray-500 text-xl" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                                 {props.title}
                             </Typography>
                         </div>
@@ -246,7 +244,7 @@ export const EventContent: React.FC = () => {
     return (
         <section id="projects" className="py-4 px-8 lg:py-20">
             <ScrollAnimation animation="fadeIn">
-                <Typography variant="h2" className="text-center font-bold text-blue-gray-900 mb-12">
+                <Typography variant="h2" className="text-center font-bold text-blue-gray-900 mb-12" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                     Proyectos en Progreso
                 </Typography>
             </ScrollAnimation>
@@ -276,7 +274,7 @@ export const SponsoredBy: React.FC = () => {
         <section className="py-8 px-8 lg:py-20">
             <div className="container mx-auto text-center">
                 <ScrollAnimation animation="fadeIn">
-                    <Typography variant="h6" color="blue-gray" className="mb-8">
+                    <Typography variant="h6" color="blue-gray" className="mb-8" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                         NUESTROS SOCIOS
                     </Typography>
                 </ScrollAnimation>
@@ -334,10 +332,10 @@ export const Faq: React.FC = () => {
             <div className="container mx-auto">
                 <ScrollAnimation animation="fadeIn">
                     <div className="text-center">
-                        <Typography variant="h1" color="blue-gray" className="mb-4">
+                        <Typography variant="h1" color="blue-gray" className="mb-4" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                             Preguntas Frecuentes
                         </Typography>
-                        <Typography variant="lead" className="mx-auto mb-24 lg:w-3/5 !text-gray-500">
+                        <Typography variant="lead" className="mx-auto mb-24 lg:w-3/5 !text-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                             Aquí encontrarás respuestas a las preguntas más comunes sobre cómo funciona Chizu,
                             cómo aprovechar sus herramientas y cómo aseguramos una experiencia confiable para desarrolladores y jugadores.
                             ¡Descubre todo lo que necesitas saber aquí! 😊
@@ -348,12 +346,12 @@ export const Faq: React.FC = () => {
                 <div className="mx-auto lg:max-w-screen-lg lg:px-20">
                     {FAQS.map(({ title, desc }, key) => (
                         <ScrollAnimation key={key} animation="fadeIn">
-                            <Accordion open={open === key + 1} onClick={() => handleOpen(key + 1)}>
-                                <AccordionHeader className="text-left text-gray-900">
+                            <Accordion open={open === key + 1} onClick={() => handleOpen(key + 1)} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                                <AccordionHeader className="text-left text-gray-900" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                                     {title}
                                 </AccordionHeader>
                                 <AccordionBody>
-                                    <Typography color="blue-gray" className="font-normal text-gray-500">
+                                    <Typography color="blue-gray" className="font-normal text-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                                         {desc}
                                     </Typography>
                                 </AccordionBody>
